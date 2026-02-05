1403310c25c4d1fae298734da68ba46194ea7040
Sean Peterson <seanpeterson.997@gmail.com>
2026-02-05 14:39:06 -0600

    Use Province/StateType fields

--- src/Form/LocationType.php
+++ src/Form/LocationType.php
@@ -3,6 +3,8 @@
 namespace OHMedia\LocationBundle\Form;
 
 use OHMedia\LocationBundle\Entity\Location;
+use OHMedia\UtilityBundle\Form\ProvinceType;
+use OHMedia\UtilityBundle\Form\StateType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
@@ -27,89 +29,16 @@ class LocationType extends AbstractType
 
         $builder->add('province');
 
-        $builder->add('provinces', ChoiceType::class, [
+        $builder->add('provinces', ProvinceType::class, [
             'label' => 'Province',
             'mapped' => false,
             'data' => $location->getProvince(),
-            'attr' => [
-                'class' => 'nice-select2',
-            ],
-            'choices' => [
-                'Alberta' => 'AB',
-                'British Columbia' => 'BC',
-                'Manitoba' => 'MB',
-                'New Brunswick' => 'NB',
-                'Newfoundland and Labrador' => 'NL',
-                'Northwest Territories' => 'NT',
-                'Nova Scotia' => 'NS',
-                'Nunavut' => 'NU',
-                'Ontario' => 'ON',
-                'Prince Edward Island' => 'PE',
-                'Quebec' => 'QC',
-                'Saskatchewan' => 'SK',
-                'Yukon' => 'YT',
-            ],
         ]);
 
-        $builder->add('states', ChoiceType::class, [
+        $builder->add('states', StateType::class, [
             'label' => 'State',
             'mapped' => false,
             'data' => $location->getProvince(),
-            'attr' => [
-                'class' => 'nice-select2',
-            ],
-            'choices' => [
-                'Alabama' => 'AL',
-                'Alaska' => 'AK',
-                'Arizona' => 'AZ',
-                'Arkansas' => 'AR',
-                'California' => 'CA',
-                'Colorado' => 'CO',
-                'Connecticut' => 'CT',
-                'Delaware' => 'DE',
-                'Florida' => 'FL',
-                'Georgia' => 'GA',
-                'Hawaii' => 'HI',
-                'Idaho' => 'ID',
-                'Illinois' => 'IL',
-                'Indiana' => 'IN',
-                'Iowa' => 'IA',
-                'Kansas' => 'KS',
-                'Kentucky' => 'KY',
-                'Louisiana' => 'LA',
-                'Maine' => 'ME',
-                'Maryland' => 'MD',
-                'Massachusetts' => 'MA',
-                'Michigan' => 'MI',
-                'Minnesota' => 'MN',
-                'Mississippi' => 'MS',
-                'Missouri' => 'MO',
-                'Montana' => 'MT',
-                'Nebraska' => 'NE',
-                'Nevada' => 'NV',
-                'New Hampshire' => 'NH',
-                'New Jersey' => 'NJ',
-                'New Mexico' => 'NM',
-                'New York' => 'NY',
-                'North Carolina' => 'NC',
-                'North Dakota' => 'ND',
-                'Ohio' => 'OH',
-                'Oklahoma' => 'OK',
-                'Oregon' => 'OR',
-                'Pennsylvania' => 'PA',
-                'Rhode Island' => 'RI',
-                'South Carolina' => 'SC',
-                'South Dakota' => 'SD',
-                'Tennessee' => 'TN',
-                'Texas' => 'TX',
-                'Utah' => 'UT',
-                'Vermont' => 'VT',
-                'Virginia' => 'VA',
-                'Washington' => 'WA',
-                'West Virginia' => 'WV',
-                'Wisconsin' => 'WI',
-                'Wyoming' => 'WY',
-            ],
         ]);
 
         $builder->add('country', CountryType::class, [
